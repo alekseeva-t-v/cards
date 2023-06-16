924fcabf3de97b5bf897aa9e6119359515a69f19
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-06-16 08:48:42 +0300

    refactor: change structure of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import Card from './components/Card';
-import './App.css';
 
+/**
+ * Основной компонент приложения, отвечающий за отображение всех элементов на странице. Дочерниq компонент Card (блок отображения карточки)
+ *
+ */
 function App() {
   return (
     <div className="App">
-      <Card img={<img src='test.jpg' className='card-img-top' alt='#'/>}>
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+      <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." href="!#">
+        <img src='test.jpg' className='card-img-top' alt='#'/>
       </Card>
 
-      <Card>
-        <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">Width supporting text bellow as a natural Lead-in to additional content</p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
+      <Card  title="Special title treatment" text="Width supporting text bellow as a natural Lead-in to additional content" href="!#">
       </Card>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,32 @@
-function Card({ img, children }) {
+import React from 'react';
+
+/**
+ * Компонент отображающий блок отображения карточки
+ *
+ * @param {object} props.children Cодержимое между открывающим и закрывающим тегом
+ * @param {string} props.title Заголовок карточки
+ * @param {string} props.text Текстовое содержимое карточки
+ * @param {string} props.href Ссылка
+ */
+const Card = (props) => {
+  const { children, title, text, href } = props;
   return (
     <div className="card">
-      {img}
-      <div className="card-body">{children}</div>
+      {children}
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <a href={href} className="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   );
-}
+};
+
+Card.defaultProps = {
+  children: null,
+  title: 'Card title',
+  text: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+  href: '!#'
+};
 
 export default Card;
